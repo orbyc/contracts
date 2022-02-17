630287c8cc4921e856e791b4eb0ef0d4a01634b7
Amaury Diaz <ad@micobo.com>
2022-02-17 17:21:26 +0100

    add testing for supply chain.

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,5 +17,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      evmVersion: "constantinople",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+  },
 };
